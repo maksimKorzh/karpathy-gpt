a580c50137b10202333ed5099ab560b1dafd74d9
Code Monkey King <freesoft.for.people@gmail.com>
2025-07-22 08:08:30 +0300

    removed examples

--- run.sh
+++ run.sh
@@ -8,11 +8,11 @@ python train.py                          \
        --compile=False                   \
        --eval_iters=20                   \
        --log_interval=1                  \
-       --block_size=128                  \
+       --block_size=12                   \
        --batch_size=12                   \
        --n_layer=6                       \
        --n_head=6                        \
        --n_embd=384                      \
-       --max_iters=2000                  \
+       --max_iters=500                  \
        --lr_decay_iters=2000             \
        --dropout=0.0
